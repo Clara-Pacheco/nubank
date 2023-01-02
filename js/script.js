aa1e6c2b127fdc6aac7d5cb57d4889d5177db567
Clara <clarapachecodev@gmail.com>
2023-01-02 17:48:50 -0300

    Implement logic for showing and hidding dropdown nav menu

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+// Change header color when scrolling
 const nav = document.getElementById('header')
-console.log(nav)
 
 window.addEventListener('scroll',() =>{
   if(this.scrollY >= 50) {
@@ -8,3 +8,25 @@ window.addEventListener('scroll',() =>{
     nav.classList.remove('active-header')
   }
 })
+
+// Menu drop down
+
+const menuItens = Array.from(document.querySelectorAll('.menu-item'))
+const dropDownMenu = Array.from(document.querySelectorAll('.dropdown-menu'))
+console.log(menuItens)
+console.log(dropDownMenu)
+
+menuItens.forEach((item,index) => {
+  item.addEventListener('click',(e)=>{
+    e.preventDefault()
+    dropDownMenu[index].classList.add('active-dropdown-menu')
+  })
+})
+
+menuItens.forEach((item,index) => {
+  item.addEventListener('mouseleave',(e)=>{
+    e.preventDefault()
+    dropDownMenu[index].classList.remove('active-dropdown-menu')
+  })
+})
+
